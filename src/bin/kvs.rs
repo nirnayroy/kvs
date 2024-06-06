f1c24d20b3fecb97c0ab09085fbad6a23ae09b04
nirnay <nirnay@enlyft.com>
2024-06-06 13:36:40 +0530

    clean commented code

--- src/bin/kvs.rs
+++ src/bin/kvs.rs
@@ -1,24 +1,9 @@
 use clap::{Parser, Subcommand};
-
-// #![feature(use_extern_macros, macro_helper_hack)]
-// #[macro_export(local_inner_macros)]
 use kvs::KvStore;
-use std::fs;
-use std::os;
-use std::fs::File;
-use std::path::Path;
 use tempfile::TempDir;
 use std::io::{Read, Write};
-use std::io;
 use serde_json::{json, Value};
-// use serde_json::to_string;
-// use assert_cmd::prelude::*;
-// use kvs::{KvStore, DBError};
-// use predicates::ord::eq;
-// use predicates::str::{contains, is_empty, PredicateStrExt};
-// use std::process::Command;
-// use tempfile::TempDir;
-// use walkdir::WalkDir;
+
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
 struct Args {
@@ -37,23 +22,13 @@ enum Commands {
     },
     #[command(about = "remove value")]
     Rm { key: String },
-    // Help
+
 }
 
 fn main() {
     let temp_dir = TempDir::new().expect("unable to create temporary working directory");
-    // println!("{}", std::env::current_dir().unwrap().display());
     let mut store = KvStore::open(&std::env::current_dir().unwrap()).unwrap();
     let args = Args::parse();
-
-    // // println!("Hello, {}!", args.cmd);
-    // let mut store = KvStore::new();
-    // // store.set("key".to_string(), "value".to_string());
-    // // println!("{:?}", store.get("key".to_string()).unwrap());
-    // // // store.remove("key".to_string());
-    // // println!("{:?}", store.get("key".to_string()).unwrap());
-
-    // // println!("{:?}", args.cmd);
 
     match args.cmd {
         Commands::Get { key } => store.get(key.to_string()),
@@ -61,49 +36,4 @@ fn main() {
         Commands::Rm { key } => store.remove(key.to_string()).map_err(|e| panic!()),
     };
 
-    // let temp_dir = TempDir::new().expect("unable to create temporary working directory");
-    // let unique_id = 0348293048;
-    // let mut file = File::create(temp_dir.path().join(unique_id.to_string())).expect("Could not create file!");
-    // let value = json!({
-    //     "previous": unique_id,
-    //     "command": "set",
-    //     "key": "value",
-    //     "value": "value",
-    // });
-    // file.write_all(value.to_string().as_bytes())
-    //     .expect("Cannot write to the file!");
-    // println!("file create");
-
-    // let mut data = String::new();
-    // let mut f = File::open(temp_dir.path().join(unique_id.to_string())).expect("Unable to open file");
-    // f.read_to_string(&mut data).expect("Unable to read string");
-    // println!("{}", data);
-    // let data_dict: serde_json::Value =
-    // serde_json::from_str(&data).expect("JSON was not well-formatted");
-
-    // println!("{:?}", find_highest_numbered_file(temp_dir.path()).unwrap().unwrap());
-    // println!("{}", data_dict["key"]);
-    // let temp_dir = TempDir::new().expect("unable to create temporary working directory");
-
-    // let mut store = KvStore::open(temp_dir.path()).unwrap();
-    // store.set("key1".to_owned(), "value1".to_owned()).unwrap();
-    // store.set("key2".to_owned(), "value2".to_owned()).unwrap();
-    // drop(store);
-    // let value = store.get("key1".to_string()).unwrap().unwrap();
-    // println!("{}", value);
-    // Command::cargo_bin("kvs")
-    //     .unwrap()
-    //     .args(&["get", "key1"])
-    //     .current_dir(&temp_dir)
-    //     .assert()
-    //     .success()
-    //     .stdout(eq("value1").trim());
-
-    // Command::cargo_bin("kvs")
-    //     .unwrap()
-    //     .args(&["get", "key2"])
-    //     .current_dir(&temp_dir)
-    //     .assert()
-    //     .success()
-    //     .stdout(eq("value2").trim());
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,9 @@
-use serde::de::value;
-use serde::Serialize;
 use serde_json::{self, json, Value};
 use std::fs;
 use std::fs::File;
 use std::io;
 use std::io::{Read, Write};
 use std::{collections::HashMap, path::Path};
-use tempfile::TempDir;
-use unique_id::sequence::SequenceGenerator;
-use unique_id::Generator;
 
 #[derive(Debug)]
 pub enum DBError {
@@ -19,13 +14,6 @@ pub enum DBError {
 }
 
 impl DBError {
-    fn from_serialization(err: serde_json::Error) -> DBError {
-        DBError::Serialize(err)
-    }
-
-    fn from_io(err: io::Error) -> DBError {
-        DBError::Io(err)
-    }
 
     fn from_log_read() -> DBError {
         DBError::Log
@@ -47,7 +35,6 @@ pub struct Log {
 
 impl Log {
     pub fn new() -> Log {
-        // let gen = SequenceGenerator::default();
         Log {
             current: 1,
             previous: 0,
@@ -100,7 +87,6 @@ impl Log {
     }
     pub fn load(&self, path: &Path) -> Result<Log, crate::io::Error>  {
         let unique_id = self.find_highest_numbered_file(path)?;
-        // println!("{}", unique_id.clone().unwrap());
         match unique_id {
             None => Ok(Log::new()),
             Some(unique_id) => Log::get_log_from_dict(&unique_id, Log::read_log_dict(&unique_id, path)),
@@ -113,7 +99,6 @@ impl Log {
         let mut f =
             File::open(path.join(unique_id)).expect("Unable to open file");
         f.read_to_string(&mut data).expect("Unable to read string");
-        // println!("{}", data);
         let data_dict: serde_json::Value =
             serde_json::from_str(&data).expect("JSON was not well-formatted");
         data_dict
@@ -131,7 +116,6 @@ impl Log {
 
 #[derive(Debug)]
 pub struct KvStore {
-    // log: String,
     dict: HashMap<String, String>,
     path: Box<Path>,
     log: Log,
@@ -139,27 +123,13 @@ pub struct KvStore {
 }
 
 impl KvStore {
-    /// Create a new KvStore.
-
-    // pub fn new() -> KvStore{
-    // let kv_store = HashMap::new();
-    //     KvStore {
-    //         // log:
-    //         store: kv_store,
-    //     }
-    // }
 
     fn recreate_state_from_log(&mut self, uid: String, target: &String) -> Result<(), DBError>{
-        // println!("recursive uid {}", uid.clone());
         if uid.to_string().parse::<u64>().unwrap() > self.max_log {
             return Ok(())
         } else {
             let log_dict = Log::read_log_dict(&uid, &*self.path);
             let command = &log_dict["command"].to_string();
-            // println!("log dict contents {}", log_dict["previous"]);
-            // println!("log dict contents {}", log_dict["command"]);
-            // println!("log dict contents {}", log_dict["value"]);
-            // println!("log dict contents {}", log_dict);
             let short = &command[1..command.len()-1];
 
             match short {
@@ -201,14 +171,9 @@ impl KvStore {
         }
 
     pub fn get(&mut self, key: String) -> Result<Option<String>, DBError> {
-        // let dict = self.store;
-        // let value = self.store.get(&key).map(|s| s.to_string()).unwrap();
-        // print!("{:?}", &*self.path);
 
         self.log = self.log.load(&*self.path).unwrap();
         self.max_log = self.log.current;
-        // println!("max log {}", self.max_log.clone());
-        // println!("path {}", self.path.display());
         self.recreate_state_from_log("2".to_string(), &key)?;
         self.log.append(("get".to_string(), json!(
             {
@@ -224,22 +189,9 @@ impl KvStore {
             println!("Key not found");
             Ok(None)
         }
-
-        // unimplemented!("unimplemented")
     }
     pub fn set(&mut self, key: String, value: String) -> Result<Option<String>, DBError> {
-        // self.store.insert((key.clone()).to_string(), value);
-        // self.store.get(&key).map(|s| s.to_string());
-        // let temp_dir = TempDir::new().expect("unable to create temporary working directory");
 
-        // let dict = HashMap::new();
-        // dict.insert((key.clone()).to_string(), value);
-        // let entry= serde_json::to_string(&p).unwrap();
-        // self.log.append(entry);
-
-        // let file = std::fs::File::open(self.path).map_err(|err| DBError::from_io(err))?;
-        // let file = std::io::BufWriter::new(file);
-        // serde_json::to_writer(file, &dict).map_err(|err| DBError::from_serialization(err))?;
         self.log = self.log.load(&*self.path).unwrap();
         self.max_log = self.log.current;
         self.log.append(("set".to_string(), json!(
@@ -253,12 +205,7 @@ impl KvStore {
     }
 
     pub fn remove(&mut self, key: String) -> Result<Option<String>, DBError> {
-        // self.store.remove(&(key.clone()).to_string());
-        // let temp_dir = TempDir::new().expect("unable to create temporary working directory");
-        // let file = std::fs::File::open(temp_dir).map_err(|err| DBError::from_io(err))?;
-        // let file = std::io::BufWriter::new(file);
-        // serde_json::to_writer(file, &self.store).map_err(|err| DBError::from_serialization(err))?;
-        // Ok(Some("".to_string()))
+
         self.log = self.log.load(&*self.path).unwrap();
         self.max_log = self.log.current;
         // println!("max log {}", self.max_log.clone());
@@ -273,7 +220,6 @@ impl KvStore {
         if self.dict.remove(&(key.clone()).to_string()).is_none(){
             println!("Key not found");
             Err(DBError::no_key())
-            // panic!()
         } else {
             Ok(Some(key))
         }
@@ -282,12 +228,6 @@ impl KvStore {
 
     pub fn open(path: &Path) -> Result<KvStore, DBError> {
 
-        // Parse the string of data into serde_json::Value.
-        // let file = std::fs::File::open(path).map_err(|err| DBError::from_io(err))?;
-        // let file = std::io::BufWriter::new(file);
-        // serde_json::to_writer(file, &init_log).map_err(|err| DBError::from_serialization(err))?;
-        // let v: serde_json::Value = serde_json::from_str(data).map_err(|err| DBError::from_serialization(err))?;
-        // let db = KvStore{store:v};
         let log_init = Log::new();
         Ok(KvStore {
             // store: kv_store,
@@ -299,11 +239,4 @@ impl KvStore {
     }
 }
 
-// trait Foo{
-//     fn get(&self) -> Self;
-//     fn set(&self) -> Self;
-//     fn remove(&self) -> Self;
-// }
-// impl Foo for KvStore{
 
-// }
